5d6be1915095dcddd30ff1e04065a255f0375320
Alisa Herr <alisa@unitymakes.us>
2019-08-28 15:19:19 -0400

    Remove staff post type

--- wp-content/themes/divi-threadcapital/functions.php
+++ wp-content/themes/divi-threadcapital/functions.php
@@ -58,97 +58,6 @@ add_action('after_setup_theme', function() {
 
 
 /**
- * Custom Post Types
- */
-add_action( 'init', function() {
-	register_post_type( __('staff', 'divi-thread'),
-		array('labels' => array(
-				'name' => __('Staff', 'divi-thread'),
-				'singular_name' => __('Staff', 'divi-thread'),
-				'add_new' => __('Add New', 'divi-thread'),
-				'add_new_item' => __('Add New Staff', 'divi-thread'),
-				'edit' => __('Edit', 'divi-thread'),
-				'edit_item' => __('Edit Staff', 'divi-thread'),
-				'new_item' => __('New Staff', 'divi-thread'),
-				'view_item' => __('View Staff', 'divi-thread'),
-				'search_items' => __('Search Staff', 'divi-thread'),
-				'not_found' => __('Nothing found in the Database.', 'divi-thread'),
-				'not_found_in_trash' => __('Nothing found in Trash', 'divi-thread'),
-				'parent_item_colon' => ''
-			), /* end of arrays */
-			'exclude_from_search' => false,
-			'publicly_queryable' => true,
-			'show_ui' => true,
-			'show_in_nav_menus' => false,
-			'menu_position' => 38,
-			'menu_icon' => 'dashicons-groups',
-			'capability_type' => 'page',
-			'hierarchical' => false,
-			'supports' => array( 'title', 'editor', 'thumbnail', 'page-attributes'),
-			'has_archive' => false,
-			'rewrite' => true,
-			'query_var' => true
-		)
-	);
-
-	register_taxonomy(
-		'staff-type',
-		'staff',
-		array(
-			'label' => __( 'Staff Type', 'divi-thread' ),
-			'rewrite' => false,
-			'hierarchical' => true,
-		)
-	);
-});
-
-/**
- * Staff list shortcode
- */
-add_shortcode('staff-listing', function($atts) {
-	extract( shortcode_atts([
-    'type' => 'staff'
-  ], $atts ) );
-
-	$staff = new WP_Query([
-		'post_type' => 'staff',
-		'posts_per_page' => -1,
-		'orderby' => 'menu_order',
-		'order' => 'ASC',
-		'tax_query' => array(
-			array(
-				'taxonomy' => 'staff-type',
-				'field'    => 'slug',
-				'terms'    => $type,
-			),
-		)
-	]);
-
-	ob_start();
-
-	if ($staff->have_posts()) : while ($staff->have_posts()) : $staff->the_post();
-		?>
-		<div class="row person" itemscope itemprop="author" itemtype="http://schema.org/Person">
-			<div class="col_5_8">
-				<h3 itemprop="name"><?php the_title(); ?></h3>
-				<div class="title" itemprop="jobTitle"><?php the_field('title'); ?></div>
-				<div><a itemprop="email" target="_blank" rel="noopener" href="mailto:<?php echo eae_encode_str(get_field('email')); ?>"><?php the_field('email'); ?></a></div>
-				<?php the_advanced_excerpt(); ?>
-			</div>
-			<div class="col_3_8">
-				<?php the_post_thumbnail('medium', ['alt' => __('Photograph of', 'divi-thread') . ' ' . get_the_title(), 'itemprop' => 'image']); ?>
-				<div class="tagline">
-					<?php the_field('tagline'); ?>
-				</div>
-			</div>
-		</div>
-		<?php
-	endwhile; endif; wp_reset_postdata();
-
-	return ob_get_clean();
-});
-
-/**
  * Team list shortcode
  */
 add_shortcode('team-listing', function($atts) {
@@ -219,6 +128,20 @@ add_shortcode('process-graphic', function($atts) {
 
 	<?php return ob_get_clean();
 });
+
+/**
+ * Filter the excerpt "read more" string.
+ *
+ * @param string $more "Read more" excerpt string.
+ * @return string (Maybe) modified "read more" excerpt string.
+ */
+add_filter( 'excerpt_more', function ( $more ) {
+	$link_open = '... <a href="' . get_the_permalink() . '">';
+  $read_more = __('Read More', 'divi-thread');
+	$link_close = '</a>';
+
+	return $link_open . $read_more . $link_close;
+} );
 
 /**
  * Add top-header widget area

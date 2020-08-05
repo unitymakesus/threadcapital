16772a548df10b8836d8ffb2fb5aeb51ce0a683a
Alisa Herr <alisa@unitywebagency.com>
2020-08-05 14:57:31 -0400

    Enqueue default style.css file for hotfixes

--- wp-content/themes/divi-threadcapital/functions.php
+++ wp-content/themes/divi-threadcapital/functions.php
@@ -11,6 +11,7 @@ add_action('wp_enqueue_scripts', function () {
 	$theme_version = et_get_theme_version();
 	wp_enqueue_style('divi/style', get_template_directory_uri() . '/style.css', false, $theme_version);
 	wp_enqueue_style('ncruralcenter/style', get_stylesheet_directory_uri() . '/css/style.css', false, null);
+	wp_enqueue_style('ncruralcenter/extras', get_stylesheet_directory_uri() . '/style.css', false, null);
   wp_enqueue_script('ncruralcenter/scripts', get_stylesheet_directory_uri() . '/scripts/main.js', false, null, true);
 }, 100);
 
